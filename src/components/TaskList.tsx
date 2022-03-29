269b17bfa51dd159fa3cc3c9dbe2ce5ab32b30a8
Jonycs <jonatascstambouli@gmail.com>
2022-03-29 11:43:33 -0300

    Change check-box

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,7 +30,11 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const change = tasks.map(task => task.id === id? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+    setTasks(change);
   }
 
   function handleRemoveTask(id: number) {
